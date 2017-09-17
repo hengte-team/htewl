599376569286c38bcf6bd8ca11219292b715a3f1
Rabin <zhangkunch@qq.com>
2017-09-17 16:15:05 +0800

    新建项目

--- application/views/m/home/index.php
+++ application/views/m/home/index.php
@@ -1,3 +1,16 @@
 <?php $this->load->view('layout/header');?>
-
+<div class="user-content">
+	<a href="http://m.bzezt.com/">
+		<img src="http://www.morginan.com/data/attachment/portal/201704/06/085712i7n7tfggemnoxncu.jpg" />
+	</a>
+	<a href="http://m.bzezt.com/home/discountMall/protection.html">
+		<img src="http://www.morginan.com/data/attachment/portal/201704/06/085712i7n7tfggemnoxncu.jpg" />
+	</a>
+	<a href="http://m.bzezt.com/tourism/tourismcart.html">
+		<img src="http://www.morginan.com/data/attachment/portal/201704/06/085712i7n7tfggemnoxncu.jpg" />
+	</a>
+	<a href="http://m.bzezt.com/tourism/tourismcart.html">
+		<img src="http://www.morginan.com/data/attachment/portal/201704/06/085712i7n7tfggemnoxncu.jpg" />
+	</a>
+</div>
 <?php $this->load->view('layout/footer');?>
